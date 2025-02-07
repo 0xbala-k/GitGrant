ecf820621da7cbebcdbd0e2e43192ddd1e40ef73
0xmuralik <krishna192reddy@gmail.com>
2025-02-07 14:29:20 -0700

    pay contributor

--- agents/chain.py
+++ agents/chain.py
@@ -7,12 +7,13 @@ from langgraph.checkpoint.memory import MemorySaver
 from github.agent import initialize_github_agent
 from rate_issue.agent import initialize_rating_agent
 from agent import initialize_meta_agent
+from github.contribution import get_contribution
 
 class State(TypedDict):
     # User input
     owner: str
     repo: str
-    total_budget: int
+    remaining_budget: int
     action: str
     
     # Issue that is being evaluated
@@ -23,6 +24,12 @@ class State(TypedDict):
     
     # List of dictionaries mapping issue numbers to difficulty ratings
     issues: List[Dict[int, int]]
+    
+    # Sum of all difficulty ratings  
+    rating_sum: int
+    
+    # Final message to be sent to the user or agent
+    message: str
 
 memory = MemorySaver()
 config = {"configurable": {"thread_id": "1"}}
@@ -42,12 +49,14 @@ def assign_rating(state: State):
         {"messages": [f"{state['action_items']}"]},
     )
     issues = state.get("issues", {}) 
-    issues[state["current_issue"]] = rating["messages"][-1].content
-    for issue, rating in issues.items():
-            if rating == 0:
-                current_issue = issue
-                return {"issues": issues, "current_issue": current_issue, "action": "evaluate"}
-    return {"issues": issues,"action_items":"","current_issue":0, "action":""}
+    issues[state["current_issue"]] = int(rating["messages"][-1].content)
+    sum=0
+    for issue, rating_val in issues.items():
+        sum+=int(rating_val)
+        if rating_val == 0:
+            current_issue = issue
+            return {"issues": issues, "current_issue": current_issue, "action": "evaluate"}
+    return {"issues": issues,"action_items":"","current_issue":0, "action":"", "rating_sum":sum}
 
 meta_agent,_ = initialize_meta_agent(memory, config)
 def meta_agent_routing(state: State):
@@ -66,19 +75,26 @@ def meta_agent_routing(state: State):
                 if rating == 0:
                     current_issue = issue
                     return {"issues": issues, "current_issue": current_issue, "action": "evaluate"}
+        else: 
+            print("Invalid action")
         return {"issues":issues, "action":""}
-    # elif state["action"] == "evaluate":
-    #     response =  meta_agent.invoke(
-    #         {"messages": [f"Owner:{state['owner']}, Repo:{state['repo']}, Issue:{state['current_issue']}"]},
-    #     )
-    #     print(response["messages"][-1].content)
-    #     return {"issues":[]}
-    # elif state["action"] == "send":
-    #     response =  meta_agent.invoke(
-    #         {"messages": [f"{state['action_items']}"]},
-    #     )
-    #     print(response["messages"][-1].content)
-    #     return {"issues":[]}
+        
+    elif state["action"] == "send":
+        contribution = get_contribution(owner=state["owner"], repo=state["repo"], pr=state["current_issue"])
+        if contribution["linked issue"]==0:
+            return {"message": f"PR {state['current_issue']} is not linked to any issue.", "action":"", "current_issue":0}
+        elif contribution["pr_state"]=="open":
+            return {"message": f"PR {state['current_issue']} is still open.", "action":"", "current_issue":0}
+        elif contribution["issue_state"]=="open":
+            return {"message": f"Linked issue {contribution['linked issue']} is still open.", "action":"", "current_issue":0}
+        
+        # get contribution["author"] address
+        address="0xD0A6F0F54803E50F27A6CC1741031094267AEE78"
+        response =  meta_agent.invoke(
+            {"messages": [f"Address: {address}, Amount: {(state['remaining_budget']/state['rating_sum'])*state['issues'][int(contribution['linked issue'])]}"]},
+        )
+        print(response["messages"][-1].content)
+        return {"issues":[], "current_issue":0, "action":""}
     
 def next_step(state: State):
     if state["action"] == "fetch":
@@ -104,5 +120,21 @@ workflow.add_edge("evaluate_issue", "assign_rating")
 workflow.add_conditional_edges("assign_rating", next_step, ["evaluate_issue", END])
 
 chain = workflow.compile()
-state = chain.invoke({"owner": "grafana", "repo": "grafana-app-sdk", "total_budget": 1000, "action": "fetch"},{"recursion_limit": 100})
+state={"owner": "grafana", "repo": "grafana-app-sdk", "remaining_budget": 0.0001, "action": "fetch"}
+
+state = chain.invoke(state,{"recursion_limit": 100})
 print("Final state:", state)
+# state={
+#     'owner': 'grafana', 
+#     'repo': 'grafana-app-sdk', 
+#     'remaining_budget': 0.1, 
+#     'action': '', 
+#     'current_issue': 0, 
+#     'action_items': '', 
+#     'issues': {617: 85, 559: 85, 523: 85, 522: 85, 489: 82, 460: 75, 457: 85, 455: 85, 453: 85, 375: 85, 363: 65, 353: 75, 312: 65, 292: 75, 236: 85, 201: 85, 194: 85, 193: 75, 191: 85, 187: 75, 184: 85, 163: 85, 151: 85, 88: 70, 72: 75, 48: 75, 47: 75, 39: 85, 26: 75, 19: 75}, 
+#     'rating_sum': 2392
+#     }
+state["action"] = "send"
+state["issues"][407]=85
+state["current_issue"] = 409
+state = chain.invoke(state)
